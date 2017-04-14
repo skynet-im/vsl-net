dc3d9a4ba343a755503c6348399b1029b6d97337
Daniel Lerch <daniel.p.lerch@gmail.com>
2017-04-14 12:36:00 +0000

    Release of VSL 1.0.0.0
    git-tfs-id: [https://vectordata.visualstudio.com]$/Skynet Backend/Skynet/VSL;C52

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("VSL")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("VectorData Security Layer")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("VectorData")]
 [assembly: AssemblyProduct("VSL")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © 2017 Daniel Lerch")]
+[assembly: AssemblyTrademark("VectorData")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
