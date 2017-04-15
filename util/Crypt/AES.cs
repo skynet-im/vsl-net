439642405e71917f9f34a23b8a020231dc807e7b
Daniel <daniel-lerch@live.de>
2017-04-15 20:49:52 +0000

    Beta Build 1 and big changes in VSL.Crypt
    git-tfs-id: [https://vectordata.visualstudio.com]$/Skynet Backend/Skynet/VSL;C53

--- util/Crypt/AES.cs
+++ util/Crypt/AES.cs
@@ -7,9 +7,12 @@ using System.Security.Cryptography;
 
 namespace VSL.Crypt
 {
+    /// <summary>
+    /// The simple implementation of AES in VSL
+    /// </summary>
     public static class AES
     {
-        // v9 © 2017 Daniel Lerch
+        // © 2017 Daniel Lerch
         /// <summary>
         /// Executes an AES encryption
         /// </summary>
@@ -17,28 +20,60 @@ namespace VSL.Crypt
         /// <param name="key">AES key (256 bit)</param>
         /// <param name="iv">Optional initialization vector (128 bit)</param>
         /// <returns></returns>
-        public async static Task<byte[]> Encrypt(byte[] b, byte[] key, byte[] iv = null)
+        public static byte[] Encrypt(byte[] b, byte[] key, byte[] iv = null)
         {
             if (key.Length != 32) throw new ArgumentOutOfRangeException("The key must have a length of 256 bit");
             if (iv?.Length != 16) throw new ArgumentOutOfRangeException("The initialization vector must have a length of 128 bit");
             if (iv == null) iv = new byte[16] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-            byte[] encrypted = new byte[0];
+            byte[] ciphertext = new byte[0];
             using (AesCryptoServiceProvider aes = new AesCryptoServiceProvider())
             {
                 aes.Key = key;
                 aes.IV = iv;
                 ICryptoTransform encryptor = aes.CreateEncryptor();
-                using (System.IO.MemoryStream msEncrypt = new System.IO.MemoryStream())
+                using (System.IO.MemoryStream msCiphertext = new System.IO.MemoryStream())
                 {
-                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    using (CryptoStream csEncrypt = new CryptoStream(msCiphertext, encryptor, CryptoStreamMode.Write))
                     {
-                        await csEncrypt.WriteAsync(b, 0, b.Length);
+                        csEncrypt.Write(b, 0, b.Length);
+                        csEncrypt.Close();
                     }
-                    encrypted = msEncrypt.ToArray();
+                    ciphertext = msCiphertext.ToArray();
                 }
             }
-            return encrypted;
+            return ciphertext;
         }
+        /// <summary>
+        /// Executes an AES encryption asychronously
+        /// </summary>
+        /// <param name="b">Plaintext</param>
+        /// <param name="key">AES key (256 bit)</param>
+        /// <param name="iv">Optional initialization vector (128 bit)</param>
+        /// <returns></returns>
+        public async static Task<byte[]> EncryptAsync(byte[] b, byte[] key, byte[] iv = null)
+        {
+            if (key.Length != 32) throw new ArgumentOutOfRangeException("The key must have a length of 256 bit");
+            if (iv?.Length != 16) throw new ArgumentOutOfRangeException("The initialization vector must have a length of 128 bit");
+            if (iv == null) iv = new byte[16] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            byte[] ciphertext = new byte[0];
+            using (AesCryptoServiceProvider aes = new AesCryptoServiceProvider())
+            {
+                aes.Key = key;
+                aes.IV = iv;
+                ICryptoTransform encryptor = aes.CreateEncryptor();
+                using (System.IO.MemoryStream msCiphertext = new System.IO.MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msCiphertext, encryptor, CryptoStreamMode.Write))
+                    {
+                        await csEncrypt.WriteAsync(b, 0, b.Length);
+                        csEncrypt.Close();
+                    }
+                    ciphertext = msCiphertext.ToArray();
+                }
+            }
+            return ciphertext;
+        }
+
         /// <summary>
         /// Executes an AES decryption
         /// </summary>
@@ -46,31 +81,60 @@ namespace VSL.Crypt
         /// <param name="key">AES key (256 bit)</param>
         /// <param name="iv">Optional initialization vector (128 bit)</param>
         /// <returns></returns>
-        public async static Task<byte[]> Decrypt(byte[] b, byte[] key, byte[] iv = null)
+        public static byte[] Decrypt(byte[] b, byte[] key, byte[] iv = null)
         {
             if (key.Length != 32) throw new ArgumentOutOfRangeException("The key must have a length of 256 bits");
             if (iv?.Length != 16) throw new ArgumentOutOfRangeException("The initialization vector must have a length of 128 bits");
             if (iv == null) iv = new byte[16] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-            byte[] decrypted = new byte[0];
+            byte[] plaintext = new byte[0];
             using (AesCryptoServiceProvider aes = new AesCryptoServiceProvider())
             {
                 aes.Key = key;
                 aes.IV = iv;
                 ICryptoTransform decryptor = aes.CreateDecryptor();
-                using (System.IO.MemoryStream msCipher = new System.IO.MemoryStream(b))
+                using (System.IO.MemoryStream msPlaintext = new System.IO.MemoryStream())
                 {
-                    using (CryptoStream csDecrypt = new CryptoStream(msCipher, decryptor, CryptoStreamMode.Read))
+                    using (CryptoStream csDecrypt = new CryptoStream(msPlaintext, decryptor, CryptoStreamMode.Write))
                     {
-                        using (System.IO.MemoryStream msDecrypt = new System.IO.MemoryStream())
-                        {
-                            await csDecrypt.CopyToAsync(msDecrypt);
-                            decrypted = msDecrypt.ToArray();
-                        }
+                        csDecrypt.Write(b, 0, b.Length);
+                        csDecrypt.Close();
                     }
+                    plaintext = msPlaintext.ToArray();
                 }
             }
-            return decrypted;
+            return plaintext;
         }
+        /// <summary>
+        /// Executes an AES decryption asynchronously
+        /// </summary>
+        /// <param name="b">Ciphertext</param>
+        /// <param name="key">AES key (256 bit)</param>
+        /// <param name="iv">Optional initialization vector (128 bit)</param>
+        /// <returns></returns>
+        public async static Task<byte[]> DecryptAsync(byte[] b, byte[] key, byte[] iv = null)
+        {
+            if (key.Length != 32) throw new ArgumentOutOfRangeException("The key must have a length of 256 bits");
+            if (iv?.Length != 16) throw new ArgumentOutOfRangeException("The initialization vector must have a length of 128 bits");
+            if (iv == null) iv = new byte[16] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            byte[] plaintext = new byte[0];
+            using (AesCryptoServiceProvider aes = new AesCryptoServiceProvider())
+            {
+                aes.Key = key;
+                aes.IV = iv;
+                ICryptoTransform decryptor = aes.CreateDecryptor();
+                using (System.IO.MemoryStream msPlaintext = new System.IO.MemoryStream())
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msPlaintext, decryptor, CryptoStreamMode.Write))
+                    {
+                        await csDecrypt.WriteAsync(b, 0, b.Length);
+                        csDecrypt.Close();
+                    }
+                    plaintext = msPlaintext.ToArray();
+                }
+            }
+            return plaintext;
+        }
+
         /// <summary>
         /// Generates a new 256 bit AES key
         /// </summary>
@@ -86,6 +150,7 @@ namespace VSL.Crypt
             }
             return key;
         }
+
         /// <summary>
         /// Generates a new initialization vector
         /// </summary>
