60c7fc7b2243ec29e22a4ef68e23d8b60a09833d
Daniel Lerch <daniel.p.lerch@gmail.com>
2017-04-23 14:27:01 +0000

    Changes in VSL cryptoengine
    git-tfs-id: [https://vectordata.visualstudio.com]$/Skynet Backend/Skynet/VSL;C87

--- util/Crypt/Hash.cs
+++ util/Crypt/Hash.cs
@@ -8,11 +8,35 @@ using System.Security.Cryptography;
 namespace VSL.Crypt
 {
     /// <summary>
-    /// The simple implementation of SHA256 in VSL
+    /// The simple implementation of SHA 1 and SHA256 in VSL
     /// </summary>
     public static class Hash
     {
         // © 2017 Daniel Lerch
+        /// <summary>
+        /// Computes the SHA1 hash of a string (not collision safe)
+        /// </summary>
+        /// <param name="s">string (UTF8 encoding)</param>
+        /// <returns></returns>
+        public static byte[] SHA1(string s)
+        {
+            UTF8Encoding enc = new UTF8Encoding();
+            return SHA1(enc.GetBytes(s));
+        }
+        /// <summary>
+        /// Computes the SHA1 hash of a byte array (not collision safe)
+        /// </summary>
+        /// <param name="b">byte array to hash</param>
+        /// <returns></returns>
+        public static byte[] SHA1(byte[] b)
+        {
+            byte[] hash;
+            using (SHA1CryptoServiceProvider sha = new SHA1CryptoServiceProvider())
+            {
+                hash = sha.ComputeHash(b);
+            }
+            return hash;
+        }
         /// <summary>
         /// Computes the SHA256 hash of a string
         /// </summary>
