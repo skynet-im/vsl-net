3fd5465c95f17fa544dc09a0bf3a534a61bcdfd2
Daniel Lerch <daniel.p.lerch@gmail.com>
2017-05-09 10:12:12 +0000

    Worked at the client and VSL naming convention improved
    git-tfs-id: [https://vectordata.visualstudio.com]$/Skynet Backend/Skynet/VSL;C142

--- VSLClient.cs
+++ VSLClient.cs
@@ -41,6 +41,24 @@ namespace VSL
         /// <param name="port">Port</param>
         /// <param name="serverKey">Public RSA key of the server</param>
         /// <returns></returns>
+        public Task ConnectAsync(string address, int port, string serverKey)
+        {
+            // <check args
+            if (string.IsNullOrEmpty(address)) throw new ArgumentNullException();
+            if (port < 0 || port > 65535) throw new ArgumentOutOfRangeException();
+            if (string.IsNullOrEmpty(serverKey)) throw new ArgumentNullException();
+            // check args>
+            return channel.Connect(address, port, serverKey);
+        }
+
+        /// <summary>
+        /// Connects the TCP Client asynchronously
+        /// </summary>
+        /// <param name="address">IP address or hostname</param>
+        /// <param name="port">Port</param>
+        /// <param name="serverKey">Public RSA key of the server</param>
+        /// <returns></returns>
+        [Obsolete("VSLClient.Connect(string, int, string) is deprecated, please use VSLClient.ConnectAsync(string, int, string) instead.", false)]
         public Task Connect(string address, int port, string serverKey)
         {
             // <check args
