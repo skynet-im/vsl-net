c1351659ca7b97bad4920c60e0aa259d50bb6ae7
Daniel <daniel-lerch@live.de>
2017-05-15 10:32:56 +0000

    Last action didn't work
    git-tfs-id: [https://vectordata.visualstudio.com]$/Skynet Backend/Skynet/VSL;C157

--- VSLTest/frmMain.Designer.cs
+++ VSLTest/frmMain.Designer.cs
@@ -28,20 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(30, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(148, 20);
+            this.textBox1.TabIndex = 0;
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textBox1);
             this.Name = "frmMain";
             this.Text = "VSL Test Application";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
