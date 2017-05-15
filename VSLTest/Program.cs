032808611a815a464f7f5b30920d6acd1edd7415
Daniel <daniel-lerch@live.de>
2017-05-15 15:28:34 +0000

    VSL test application
    git-tfs-id: [https://vectordata.visualstudio.com]$/Skynet Backend/Skynet/VSL;C160

--- VSLTest/Program.cs
+++ VSLTest/Program.cs
@@ -16,7 +16,7 @@ namespace VSLTest
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmMain());
         }
     }
 }
