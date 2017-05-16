b07f600cd6de4af82ee6f5892a814af570ae1d05
Daniel Lerch <daniel.p.lerch@gmail.com>
2017-05-16 07:00:53 +0000

    VSL packet 01 KeyExchange
    git-tfs-id: [https://vectordata.visualstudio.com]$/Skynet Backend/Skynet/VSL;C162

--- VSL/net/PacketHandlerClient.cs
+++ VSL/net/PacketHandlerClient.cs
@@ -21,6 +21,14 @@ namespace VSL
         //  constructor>
 
         // <functions
+        internal override void HandlePacket00Handshake(Packet00Handshake p)
+        {
+            throw new NotImplementedException();
+        }
+        internal override void HandlePacket01KeyExchange(Packet01KeyExchange p)
+        {
+            throw new NotImplementedException();
+        }
         internal override void HandlePacket255Accepted(Packet255Accepted p)
         {
             if (p.Accepted) throw new NotImplementedException();
---
+++ VSL/packet/Packet01KeyExchange.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSL
+{
+    internal class Packet01KeyExchange : IPacket
+    {
+        internal byte[] AesKey;
+        internal byte[] ClientIV;
+        internal byte[] ServerIV;
+        internal ushort LatestVSL;
+        internal ushort OldestVSL;
+        internal ushort LatestProduct;
+        internal ushort OldestProduct;
+
+        internal Packet01KeyExchange()
+        {
+
+        }
+
+        internal Packet01KeyExchange(byte[] aesKey, byte[] clientIV, byte[] serverIV, ushort latestVSL, ushort oldestVSL, ushort latestProduct, ushort oldestProduct)
+        {
+            AesKey = aesKey;
+            ClientIV = clientIV;
+            ServerIV = serverIV;
+            LatestVSL = latestVSL;
+            OldestVSL = oldestVSL;
+            LatestProduct = latestProduct;
+            OldestProduct = oldestProduct;
+        }
+
+        public byte ID
+        {
+            get
+            {
+                return 1;
+            }
+        }
+
+        public IPacket CreatePacket(byte[] buf)
+        {
+            Packet01KeyExchange packet = new Packet01KeyExchange();
+            packet.ReadPacket(buf);
+            return packet;
+        }
+
+        public void HandlePacket(PacketHandler handler)
+        {
+            handler.HandlePacket01KeyExchange(this);
+        }
+
+        public void ReadPacket(byte[] buf)
+        {
+            PacketBuffer reader = new PacketBuffer(buf);
+            AesKey = reader.ReadByteArray(32);
+            ClientIV = reader.ReadByteArray(16);
+            ServerIV = reader.ReadByteArray(16);
+            LatestVSL = reader.ReadUShort();
+            OldestVSL = reader.ReadUShort();
+            LatestProduct = reader.ReadUShort();
+            OldestProduct = reader.ReadUShort();
+        }
+
+        public byte[] WritePacket()
+        {
+            PacketBuffer writer = new PacketBuffer();
+            writer.WriteByteArray(AesKey, false);
+            writer.WriteByteArray(ClientIV, false);
+            writer.WriteByteArray(ServerIV, false);
+            writer.WriteUShort(LatestVSL);
+            writer.WriteUShort(OldestVSL);
+            writer.WriteUShort(LatestProduct);
+            writer.WriteUShort(OldestProduct);
+            return writer.ToArray();
+        }
+    }
+}
