bf9f810fa0dc826f01c9c28cd3f80953830833a7
Daniel <daniel-lerch@live.de>
2017-06-02 11:02:21 +0000

    VSL: FileTransferProgress event added
    git-tfs-id: [https://vectordata.visualstudio.com]$/Skynet Backend/Skynet/VSL;C215

---
+++ VSL/FileTransfer/Events.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSL.FileTransfer
+{
+    /// <summary>
+    /// Event data when the file transfer progress has changed.
+    /// </summary>
+    public class FileTransferProgressEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Gets a value indicating the percentage of transfered to total bytes. The value reaches from 0 (header transfered) to 1 (download finished).
+        /// </summary>
+        public double Percentage { get; }
+    }
+}
