7e403166112f5fd6188acbbf0d8647b758f43196
Daniel <daniel-lerch@live.de>
2017-08-13 20:08:09 +0000

    VSL: Efficiency improve of AES started
    git-tfs-id: [https://vectordata.visualstudio.com]$/Skynet Backend/Skynet/VSL;C349

--- VSL/util/Crypt/AES.cs
+++ VSL/util/Crypt/AES.cs
@@ -10,9 +10,59 @@ namespace VSL.Crypt
     /// <summary>
     /// The simple implementation of AES in VSL
     /// </summary>
-    public static class AES
+    public class AES : IDisposable
     {
         // © 2017 Daniel Lerch
+        private AesCryptoServiceProvider csp;
+        private ICryptoTransform encryptor;
+        private ICryptoTransform decryptor;
+        /// <summary>
+        /// Initializes a new instance of the AES class. This should be more efficient than the static methods for multiple operations with the same key.
+        /// </summary>
+        /// <param name="key">AES key (256 bit)</param>
+        /// <param name="iv">Optional initialization vector (128 bit)</param>
+        /// <exception cref="ArgumentOutOfRangeException"/>
+        public AES(byte[] key, byte[] iv = null)
+        {
+            if (key.Length != 32) throw new ArgumentOutOfRangeException("The key must have a length of 256 bit");
+            if (iv == null) iv = new byte[16] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            if (iv.Length != 16) throw new ArgumentOutOfRangeException("The initialization vector must have a length of 128 bit");
+            csp = new AesCryptoServiceProvider()
+            {
+                Key = key,
+                IV = iv
+            };
+            encryptor = csp.CreateEncryptor();
+            decryptor = csp.CreateDecryptor();
+        }
+        /// <summary>
+        /// Sets the key for the next operations.
+        /// </summary>
+        public byte[] Key
+        {
+            set
+            {
+                if (value.Length != 32) throw new ArgumentOutOfRangeException("The key must have a length of 256 bit");
+                csp.Key = value;
+                encryptor = csp.CreateEncryptor();
+                decryptor = csp.CreateDecryptor();
+            }
+        }
+        /// <summary>
+        /// Sets the initialization vector for the next operations.
+        /// </summary>
+        public byte[] IV
+        {
+            set
+            {
+                if (value == null) value = new byte[16] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+                if (value.Length != 16) throw new ArgumentOutOfRangeException("The initialization vector must have a length of 128 bit");
+                csp.IV = value;
+                encryptor = csp.CreateEncryptor();
+                decryptor = csp.CreateDecryptor();
+            }
+        }
+
         /// <summary>
         /// Executes an AES encryption
         /// </summary>
@@ -148,7 +198,7 @@ namespace VSL.Crypt
         }
 
         /// <summary>
-        /// Generates a new initialization vector
+        /// Generates a new initialization vector.
         /// </summary>
         /// <returns></returns>
         public static byte[] GenerateIV()
@@ -161,5 +211,72 @@ namespace VSL.Crypt
             }
             return iv;
         }
+        /// <summary>
+        /// Generates a new initialization vector.
+        /// </summary>
+        /// <param name="set">Specifies whether the new IV should be set for the next operations.</param>
+        /// <returns></returns>
+        public byte[] GenerateIV(bool set)
+        {
+            if (set)
+            {
+                csp.GenerateIV();
+                IV = csp.IV;
+                return csp.IV;
+            }
+            else
+            {
+                byte[] oldIV = csp.IV;
+                csp.GenerateIV();
+                byte[] newIV = csp.IV;
+                csp.IV = oldIV;
+                return newIV;
+            }
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        /// <param name="disposing">Specify whether managed objects should be disposed.</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).
+                    csp.Dispose();
+                    encryptor.Dispose();
+                    decryptor.Dispose();
+                }
+
+                // -TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // -TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // -TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~AES() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // -TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
     }
 }
