aaf90a01514ab083ef76ab69cc0d18e0301a2dee
Daniel Lerch <daniel.p.lerch@gmail.com>
2017-08-26 20:59:21 +0000

    Server v4.0.1.12 released (bugfixes)
    git-tfs-id: [https://vectordata.visualstudio.com]$/Skynet Backend/Skynet/VSL;C410

--- VSLTest/Client.cs
+++ VSLTest/Client.cs
@@ -17,21 +17,20 @@ namespace VSLTest
         public Client(TcpClient native)
         {
             Vsl = new VSLServer(native, 0, 0, Program.Keypair, ThreadMgr.InvokeMode.Dispatcher);
-            Vsl.Logger.PrintDebugMessages = true;
-            Vsl.Logger.PrintExceptionMessages = true;
-            Vsl.Logger.PrintInfoMessages = true;
+            //Vsl.Logger.PrintDebugMessages = true;
+            //Vsl.Logger.PrintExceptionMessages = true;
+            //Vsl.Logger.PrintInfoMessages = true;
+            Vsl.Logger.InvokeDebugMessages = false;
+            Vsl.Logger.InvokeExceptionMessages = false;
+            Vsl.Logger.InvokeInfoMessages = false;
             Vsl.FileTransfer.FileTransferRequested += Vsl_FileTransferRequested;
             Vsl.ConnectionClosed += Vsl_ConnectionClosed;
         }
 
         private void Vsl_ConnectionClosed(object sender, ConnectionClosedEventArgs e)
         {
-            MessageBox.Show(string.Format("[Server] Connection closed\r\nReason: {0}\r\nReceived: {1}\r\nSent: {2}", e.Reason, e.ReceivedBytes, e.SentBytes));
-            for (int i = 0; i < Program.Clients.Length; i++)
-            {
-                if (Program.Clients[i] == this)
-                    Program.Clients[i] = null;
-            }
+            //MessageBox.Show(string.Format("[Server] Connection closed\r\nReason: {0}\r\nReceived: {1}\r\nSent: {2}", e.Reason, e.ReceivedBytes, e.SentBytes));
+            Program.Clients = Program.Clients.Remove(this);
         }
 
         private void Vsl_FileTransferRequested(object sender, EventArgs e)
