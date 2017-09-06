5b6792a6074d51240407a027cede6ac39dee51e8
Daniel Lerch <daniel.p.lerch@gmail.com>
2017-09-06 12:14:59 +0000

    VSL: Worked at ThreadSafeList<T>
    git-tfs-id: [https://vectordata.visualstudio.com]$/Skynet Backend/Skynet/VSL;C450

--- VSL/util/ThreadSafeList.cs
+++ VSL/util/ThreadSafeList.cs
@@ -6,29 +6,78 @@ using System.Threading.Tasks;
 
 namespace VSL
 {
+    /// <summary>
+    /// Represents a strongly typed, threadsafe list of refernce class objects that can't be accessed by index.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class ThreadSafeList<T> where T : class
     {
         private List<T> currentList;
+        private List<T> addToList;
+        private List<T> removeFromList;
         private bool cleaning = false;
         private object cleanupLock;
         private object changeStateLock;
+        private object currentListLock;
+        private object addToListLock;
+        private object removeFromListLock;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ThreadSafeList{T}"/> class.
+        /// </summary>
         public ThreadSafeList()
         {
             currentList = new List<T>();
+            addToList = new List<T>();
+            removeFromList = new List<T>();
             cleanupLock = new object();
             changeStateLock = new object();
+            currentListLock = new object();
+            addToListLock = new object();
+            removeFromListLock = new object();
         }
+        /// <summary>
+        /// Adds an object to the <see cref="ThreadSafeList{T}"/>. 
+        /// </summary>
+        /// <param name="item">The item to be added to the <see cref="ThreadSafeList{T}"/>. The value can't be null.</param>
+        /// <exception cref="ArgumentNullException"/>
         public void Add(T item)
         {
-            currentList.Add(item);
+            if (item == null)
+                throw new ArgumentNullException("item");
+            lock (currentListLock)
+                currentList.Add(item);
+            bool cleaning;
+            lock (changeStateLock)
+                cleaning = this.cleaning;
+            if (cleaning)
+                lock (addToListLock)
+                    addToList.Add(item);
         }
-        public void Remove(T item)
+        /// <summary>
+        /// Removes all occurencies of a specific object from the <see cref="ThreadSafeList{T}"/>
+        /// </summary>
+        /// <param name="item">The object to be removed from the <see cref="ThreadSafeList{T}"/>. The value can't be null.</param>
+        /// <returns></returns>
+        public bool Remove(T item)
         {
+            if (item == null)
+                throw new ArgumentNullException("item");
+            bool final = false;
             for (int i = 0; i < currentList.Count; i++)
             {
                 if (ReferenceEquals(currentList[i], item))
+                {
                     currentList[i] = null;
+                    final = true;
+                }
             }
+            bool cleaning;
+            lock (changeStateLock)
+                cleaning = this.cleaning;
+            if (cleaning)
+                lock (removeFromListLock)
+                    removeFromList.Add(item);
+            return final;
         }
         public void Cleanup()
         {
@@ -37,7 +86,15 @@ namespace VSL
                 List<T> newList = new List<T>();
                 lock (changeStateLock)
                     cleaning = true;
-
+                ForEach((type) => newList.Add(type));
+                lock (changeStateLock)
+                {
+                    newList.AddRange(addToList);
+                    foreach (T item in removeFromList)
+                        newList.Remove(item);
+                    currentList = newList;
+                    cleaning = false;
+                }
             }
         }
         /// <summary>
