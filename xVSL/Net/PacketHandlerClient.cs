7203c5f3db22093c52000fd02765e114d11057e4
Daniel <daniel-lerch@live.de>
2017-10-01 21:25:46 +0000

    VSL as shared project stage 2
    git-tfs-id: [https://vectordata.visualstudio.com]$/Skynet Backend/Skynet/VSL;C498

---
+++ xVSL/Net/PacketHandlerClient.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VSL.Packet;
+
+namespace VSL
+{
+    internal class PacketHandlerClient : PacketHandler
+    {
+        // <fields
+        new internal VSLClient parent;
+        //  fields>
+
+        // <constructor
+        internal PacketHandlerClient(VSLClient parent)
+        {
+            this.parent = parent;
+            base.parent = parent;
+            InitializeComponent();
+        }
+        //  constructor>
+
+        // <functions
+        internal override bool HandleP00Handshake(P00Handshake p)
+        {
+            parent.ExceptionHandler.CloseConnection("InvalidPacket", "VSL clients can not handle P00Handshake.");
+            return false;
+        }
+        internal override bool HandleP01KeyExchange(P01KeyExchange p)
+        {
+            parent.ExceptionHandler.CloseConnection("InvalidPacket", "VSL clients can not handle P01KeyExchange.");
+            return false;
+        }
+        internal override bool HandleP02Certificate(P02Certificate p)
+        {
+            throw new NotSupportedException("This VSL version does not support Certificates");
+        }
+        internal override bool HandleP03FinishHandshake(P03FinishHandshake p)
+        {
+            switch (p.ConnectionType)
+            {
+                case ConnectionType.Compatible:
+                    parent.OnConnectionEstablished();
+                    return true;
+                case ConnectionType.Redirect:
+                    parent.ExceptionHandler.CloseConnection("NotSupported", "This VSL version does not support redirects.");
+                    return false;
+                case ConnectionType.NotCompatible:
+                    parent.ExceptionHandler.CloseConnection("ConnectionDenied", "The specified server denied the connection to this VSL/application version client.");
+                    return false;
+                default:
+                    return false;
+            }
+        }
+        internal override bool HandleP04ChangeIV(P04ChangeIV p)
+        {
+            parent.ExceptionHandler.CloseConnection("InvalidPacket", "VSL clients can not handle P04ChangeIV.");
+            return false;
+        }
+        internal override bool HandleP05KeepAlive(P05KeepAlive p)
+        {
+            parent.ExceptionHandler.CloseConnection("InvalidPacket", "VSL clients can not handle P05KeepAlive.");
+            return false;
+        }
+        // overriding void HandleP06Accepted is not neccessary.
+        internal override bool HandleP07OpenFileTransfer(P07OpenFileTransfer p)
+        {
+            parent.ExceptionHandler.CloseConnection("InvalidPacket", "VSL clients can not handle P07OpenFileTransfer.");
+            return false;
+        }
+        // overriding void HandleP08FileHeader is not neccessary.
+        // overriding void HandleP09FileDataBlock is not neccessary.
+        //  functions>
+    }
+}
---
+++ xVSL/Packet/IPacket.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSL.Packet
+{
+    /// <summary>
+    /// Represents an internal VSL packet
+    /// </summary>
+    internal interface IPacket
+    {
+        /// <summary>
+        /// Returns the packet ID.
+        /// </summary>
+        byte PacketID { get; }
+        /// <summary>
+        /// Returns the length characteristics of the packet.
+        /// </summary>
+        PacketLength PacketLength { get; }
+        /// <summary>
+        /// Creates a new packet of the specified instance.
+        /// </summary>
+        /// <returns></returns>
+        IPacket New();
+        /// <summary>
+        /// Forwards the packet to the handler.
+        /// </summary>
+        /// <param name="handler">The responsible packet handler.</param>
+        /// <exception cref="InvalidOperationException"/>
+        /// <exception cref="NotImplementedException"/>
+        /// <exception cref="NotSupportedException"/>
+        /// <returns>If packet handling succeeded.</returns>
+        bool HandlePacket(PacketHandler handler);
+        /// <summary>
+        /// Reads the data from a PacketBuffer.
+        /// </summary>
+        /// <param name="buf">PacketBuffer to read the packet content.</param>
+        /// <exception cref="ArgumentOutOfRangeException"/>
+        void ReadPacket(PacketBuffer buf);
+        /// <summary>
+        /// Writes the data to a PacketBuffer.
+        /// </summary>
+        /// <param name="buf">PacketBuffer to write the packet content.</param>
+        void WritePacket(PacketBuffer buf);
+    }
+}
