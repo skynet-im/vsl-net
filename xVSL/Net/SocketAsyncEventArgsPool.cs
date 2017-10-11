bd2c563a4e4ced46892192b95c2a15a9fe2a875a
Daniel Lerch <daniel.p.lerch@gmail.com>
2017-10-11 16:41:49 +0000

    VSL: Async socket receive
    git-tfs-id: [https://vectordata.visualstudio.com]$/Skynet Backend/Skynet/VSL;C507

---
+++ xVSL/Net/SocketAsyncEventArgsPool.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace VSL
+{
+    internal sealed class SocketAsyncEventArgsPool
+    {
+        private ConcurrentBag<SocketAsyncEventArgs> bag;
+        internal SocketAsyncEventArgsPool()
+        {
+            bag = new ConcurrentBag<SocketAsyncEventArgs>();
+        }
+    }
+}
