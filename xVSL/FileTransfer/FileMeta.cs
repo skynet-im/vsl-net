5e3dfa6e328b79fbbd02360ce93a2fa75f626aae
Daniel Lerch <daniel.p.lerch@gmail.com>
2017-12-04 15:55:57 +0000

    VSL: FileTransfer rewrite stage 1
    git-tfs-id: [https://vectordata.visualstudio.com]$/Skynet Backend/Skynet/VSL;C559

---
+++ xVSL/FileTransfer/FileMeta.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VSL.FileTransfer
+{
+    public class FileMeta
+    {
+        // TODO: ContentAlgorithm
+
+        /// <summary>
+        /// Gets whether plain data is available and the properties and fields can be used.
+        /// </summary>
+        public bool Available { get; }
+        /// <summary>
+        /// Gets encrypted or unencrypted binary data that can be restored.
+        /// </summary>
+        public byte[] BinaryData { get; }
+    }
+}
