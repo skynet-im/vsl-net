60ec12eced542beabea6cdd1d55a72f94a170970
Daniel <daniel-lerch@live.de>
2018-01-16 09:42:32 +0000

    VSL: Rewrite of the static Hash API
    git-tfs-id: [https://vectordata.visualstudio.com]$/Skynet Backend/VSL/dev;C638

---
+++ CryptTest/EcdhControl.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VSL.Crypt;
+
+namespace CryptTest
+{
+    public partial class EcdhControl : UserControl
+    {
+        public EcdhControl()
+        {
+            InitializeComponent();
+        }
+
+        private void btnECDHAliceGenParams_Click(object sender, EventArgs e)
+        {
+            ECDH.GenerateKey(out byte[] privateKey, out byte[] publicKey);
+            TbAlicePrivate.Text = Util.ToHexString(privateKey);
+            TbAlicePublic.Text = Util.ToHexString(publicKey);
+        }
+
+        private void btnECDHBobGenParams_Click(object sender, EventArgs e)
+        {
+            ECDH.GenerateKey(out byte[] privateKey, out byte[] publicKey);
+            TbBobPrivate.Text = Util.ToHexString(privateKey);
+            TbBobPublic.Text = Util.ToHexString(publicKey);
+        }
+
+        private void btnECDHCreateKey_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(TbAlicePrivate.Text) || string.IsNullOrEmpty(TbAlicePublic.Text))
+                btnECDHAliceGenParams_Click(null, null);
+
+            if (string.IsNullOrEmpty(TbBobPrivate.Text) || string.IsNullOrEmpty(TbBobPublic.Text))
+                btnECDHBobGenParams_Click(null, null);
+
+            byte[] alicePrivate = Util.GetBytes(TbAlicePrivate.Text);
+            byte[] alicePublic = Util.GetBytes(TbAlicePublic.Text);
+
+            byte[] bobPrivate = Util.GetBytes(TbBobPrivate.Text);
+            byte[] bobPublic = Util.GetBytes(TbBobPublic.Text);
+
+            TbAliceKey.Text = Util.ToHexString(ECDH.DeriveKey(alicePrivate, bobPublic));
+            TbBobKey.Text = Util.ToHexString(ECDH.DeriveKey(bobPrivate, alicePublic));
+        }
+    }
+}
--- CryptTest/MainForm.Designer.cs
+++ CryptTest/MainForm.Designer.cs
@@ -30,15 +30,21 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.aesTab = new System.Windows.Forms.TabPage();
-            this.e2eFileTab = new System.Windows.Forms.TabPage();
             this.aesControl1 = new CryptTest.AesControl();
-            this.e2EFileControl1 = new CryptTest.E2EFileControl();
             this.rsaTab = new System.Windows.Forms.TabPage();
             this.rsaControl1 = new CryptTest.RsaControl();
+            this.e2eFileTab = new System.Windows.Forms.TabPage();
+            this.e2EFileControl1 = new CryptTest.E2EFileControl();
+            this.shaTab = new System.Windows.Forms.TabPage();
+            this.shaControl1 = new CryptTest.ShaControl();
+            this.ecdhTab = new System.Windows.Forms.TabPage();
+            this.ecdhControl1 = new CryptTest.EcdhControl();
             this.tabControl1.SuspendLayout();
             this.aesTab.SuspendLayout();
-            this.e2eFileTab.SuspendLayout();
             this.rsaTab.SuspendLayout();
+            this.e2eFileTab.SuspendLayout();
+            this.shaTab.SuspendLayout();
+            this.ecdhTab.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -46,6 +52,8 @@
             this.tabControl1.Controls.Add(this.aesTab);
             this.tabControl1.Controls.Add(this.rsaTab);
             this.tabControl1.Controls.Add(this.e2eFileTab);
+            this.tabControl1.Controls.Add(this.shaTab);
+            this.tabControl1.Controls.Add(this.ecdhTab);
             this.tabControl1.Location = new System.Drawing.Point(12, 12);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
@@ -63,31 +71,12 @@
             this.aesTab.Text = "AES";
             this.aesTab.UseVisualStyleBackColor = true;
             // 
-            // e2eFileTab
-            // 
-            this.e2eFileTab.Controls.Add(this.e2EFileControl1);
-            this.e2eFileTab.Location = new System.Drawing.Point(4, 22);
-            this.e2eFileTab.Name = "e2eFileTab";
-            this.e2eFileTab.Padding = new System.Windows.Forms.Padding(3);
-            this.e2eFileTab.Size = new System.Drawing.Size(402, 231);
-            this.e2eFileTab.TabIndex = 0;
-            this.e2eFileTab.Text = "E2E File";
-            this.e2eFileTab.UseVisualStyleBackColor = true;
-            // 
             // aesControl1
             // 
             this.aesControl1.Location = new System.Drawing.Point(0, 0);
             this.aesControl1.Name = "aesControl1";
             this.aesControl1.Size = new System.Drawing.Size(402, 231);
             this.aesControl1.TabIndex = 0;
-            // 
-            // e2EFileControl1
-            // 
-            this.e2EFileControl1.BackColor = System.Drawing.Color.Transparent;
-            this.e2EFileControl1.Location = new System.Drawing.Point(0, 0);
-            this.e2EFileControl1.Name = "e2EFileControl1";
-            this.e2EFileControl1.Size = new System.Drawing.Size(402, 231);
-            this.e2EFileControl1.TabIndex = 0;
             // 
             // rsaTab
             // 
@@ -106,6 +95,59 @@
             this.rsaControl1.Size = new System.Drawing.Size(402, 231);
             this.rsaControl1.TabIndex = 0;
             // 
+            // e2eFileTab
+            // 
+            this.e2eFileTab.Controls.Add(this.e2EFileControl1);
+            this.e2eFileTab.Location = new System.Drawing.Point(4, 22);
+            this.e2eFileTab.Name = "e2eFileTab";
+            this.e2eFileTab.Padding = new System.Windows.Forms.Padding(3);
+            this.e2eFileTab.Size = new System.Drawing.Size(402, 231);
+            this.e2eFileTab.TabIndex = 0;
+            this.e2eFileTab.Text = "E2E File";
+            this.e2eFileTab.UseVisualStyleBackColor = true;
+            // 
+            // e2EFileControl1
+            // 
+            this.e2EFileControl1.BackColor = System.Drawing.Color.Transparent;
+            this.e2EFileControl1.Location = new System.Drawing.Point(0, 0);
+            this.e2EFileControl1.Name = "e2EFileControl1";
+            this.e2EFileControl1.Size = new System.Drawing.Size(402, 231);
+            this.e2EFileControl1.TabIndex = 0;
+            // 
+            // shaTab
+            // 
+            this.shaTab.Controls.Add(this.shaControl1);
+            this.shaTab.Location = new System.Drawing.Point(4, 22);
+            this.shaTab.Name = "shaTab";
+            this.shaTab.Size = new System.Drawing.Size(402, 231);
+            this.shaTab.TabIndex = 3;
+            this.shaTab.Text = "SHA";
+            this.shaTab.UseVisualStyleBackColor = true;
+            // 
+            // shaControl1
+            // 
+            this.shaControl1.Location = new System.Drawing.Point(0, 0);
+            this.shaControl1.Name = "shaControl1";
+            this.shaControl1.Size = new System.Drawing.Size(402, 231);
+            this.shaControl1.TabIndex = 0;
+            // 
+            // ecdhTab
+            // 
+            this.ecdhTab.Controls.Add(this.ecdhControl1);
+            this.ecdhTab.Location = new System.Drawing.Point(4, 22);
+            this.ecdhTab.Name = "ecdhTab";
+            this.ecdhTab.Size = new System.Drawing.Size(402, 231);
+            this.ecdhTab.TabIndex = 4;
+            this.ecdhTab.Text = "ECDH";
+            this.ecdhTab.UseVisualStyleBackColor = true;
+            // 
+            // ecdhControl1
+            // 
+            this.ecdhControl1.Location = new System.Drawing.Point(0, 0);
+            this.ecdhControl1.Name = "ecdhControl1";
+            this.ecdhControl1.Size = new System.Drawing.Size(402, 231);
+            this.ecdhControl1.TabIndex = 0;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -118,8 +160,10 @@
             this.Text = "VSL Test Anwendung für Kryptographie";
             this.tabControl1.ResumeLayout(false);
             this.aesTab.ResumeLayout(false);
-            this.e2eFileTab.ResumeLayout(false);
             this.rsaTab.ResumeLayout(false);
+            this.e2eFileTab.ResumeLayout(false);
+            this.shaTab.ResumeLayout(false);
+            this.ecdhTab.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -133,6 +177,10 @@
         private AesControl aesControl1;
         private System.Windows.Forms.TabPage rsaTab;
         private RsaControl rsaControl1;
+        private System.Windows.Forms.TabPage shaTab;
+        private ShaControl shaControl1;
+        private System.Windows.Forms.TabPage ecdhTab;
+        private EcdhControl ecdhControl1;
     }
 }
 
