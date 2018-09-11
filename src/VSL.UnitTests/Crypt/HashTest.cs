42997f75c70176f3ae3d24269db1a414fe6b098b
Daniel Lerch <daniel.p.lerch@gmail.com>
2018-09-11 17:18:05 +0200

    Crypto update stage 2; Improve unit tests

---
+++ src/VSL.UnitTests/Crypt/HashTest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text;
+using VSL.BinaryTools;
+using VSL.Crypt;
+
+namespace VSL.UnitTests.Crypt
+{
+    [TestClass]
+    public class HashTest
+    {
+        [TestMethod]
+        public void TestSha1()
+        {
+            byte[] sha1Utf8Empty = Util.GetBytes(Constants.Sha1Utf8Empty);
+            byte[] sha1 = Hash.SHA1("", Encoding.UTF8);
+            CollectionAssert.AreEqual(sha1Utf8Empty, sha1, "Empty SHA-1 hash invalid.");
+        }
+
+        [TestMethod]
+        public void TestSha256()
+        {
+            byte[] sha256Utf8Empty = Util.GetBytes(Constants.Sha256Utf8Empty);
+            byte[] sha256 = Hash.SHA256("", Encoding.UTF8);
+            CollectionAssert.AreEqual(sha256Utf8Empty, sha256, "Empty SHA-256 hash invalid.");
+        }
+
+        [DataTestMethod]
+        [DataRow("", Constants.ScryptUtf8Empty)]
+        [DataRow("Hello", Constants.ScryptUtf8Hello)]
+        public void TestScrypt(string input, string result)
+        {
+            byte[] hash = Hash.Scrypt(Encoding.UTF8.GetBytes(input), new byte[0], 16384, 8, 1, 64);
+            CollectionAssert.AreEqual(Util.GetBytes(result), hash);
+        }
+    }
+}
--- src/VSL/Crypt/RsaStatic.cs
+++ src/VSL/Crypt/RsaStatic.cs
@@ -20,11 +20,26 @@ namespace VSL.Crypt
         /// <exception cref="ArgumentNullException"></exception>
         /// <exception cref="ArgumentOutOfRangeException"></exception>
         /// <exception cref="CryptographicException"></exception>
-        /// <returns></returns>
         public static byte[] EncryptBlock(byte[] plaintext, string key)
         {
             if (plaintext == null) throw new ArgumentNullException(nameof(plaintext));
             if (string.IsNullOrEmpty(key)) throw new ArgumentNullException(nameof(key));
+            if (plaintext.Length > 214) throw new ArgumentOutOfRangeException(nameof(plaintext), plaintext.Length, "One block must measure 214 bytes");
+
+            return EncryptBlock(plaintext, new RSAParameters().ImportXmlKey(key));
+        }
+
+        /// <summary>
+        /// Encrypts one block using RSA with OAEP.
+        /// </summary>
+        /// <param name="plaintext">Data to encrypt (max. 214 bytes).</param>
+        /// <param name="key"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        /// <exception cref="CryptographicException"></exception>
+        public static byte[] EncryptBlock(byte[] plaintext, RSAParameters key)
+        {
+            if (plaintext == null) throw new ArgumentNullException(nameof(plaintext));
             if (plaintext.Length > 214) throw new ArgumentOutOfRangeException(nameof(plaintext), plaintext.Length, "One block must measure 214 bytes");
             using (var rsa = CreateProvider(key))
             {
@@ -39,34 +54,40 @@ namespace VSL.Crypt
         /// <param name="key">Public key as xml string.</param>
         /// <exception cref="ArgumentNullException"></exception>
         /// <exception cref="CryptographicException"></exception>
-        /// <returns></returns>
         public static byte[] Encrypt(byte[] plaintext, string key)
         {
             if (plaintext == null) throw new ArgumentNullException(nameof(plaintext));
             if (string.IsNullOrEmpty(key)) throw new ArgumentNullException(nameof(key));
 
+            return Encrypt(plaintext, new RSAParameters().ImportXmlKey(key));
+        }
+
+        /// <summary>
+        /// Encrypts data using RSA with OAEP.
+        /// </summary>
+        /// <param name="plaintext">Data to encrypt with any length.</param>
+        /// <param name="key"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="CryptographicException"></exception>
+        public static byte[] Encrypt(byte[] plaintext, RSAParameters key)
+        {
+            if (plaintext == null) throw new ArgumentNullException(nameof(plaintext));
+
+            int blocks = Util.GetTotalSize(plaintext.Length, 214) / 214;
+            byte[] ciphertext = new byte[blocks * 256];
             using (RSA rsa = CreateProvider(key))
             {
-                if (plaintext.Length <= 214)
+                Parallel.For(0, blocks - 1, (i) =>
                 {
-                    return EncryptInternal(rsa, plaintext);
-                }
-                else
-                {
-                    int blocks = Util.GetTotalSize(plaintext.Length, 214) / 214;
-                    byte[] ciphertext = new byte[blocks * 256];
-                    Parallel.For(0, blocks - 1, (i) =>
-                    {
-                        byte[] buf = EncryptInternal(rsa, plaintext.TakeAt(i * 214, 214));
-                        Array.Copy(buf, 0, ciphertext, i * 256, 256);
-                    });
-                    int lidx = (blocks - 1) * 214;
-                    int llen = plaintext.Length - lidx;
-                    byte[] lbuf = EncryptInternal(rsa, plaintext.TakeAt(lidx, llen));
-                    Array.Copy(lbuf, 0, ciphertext, (blocks - 1) * 256, 256);
-                    return ciphertext;
-                }
+                    byte[] buf = EncryptInternal(rsa, plaintext.TakeAt(i * 214, 214));
+                    Array.Copy(buf, 0, ciphertext, i * 256, 256);
+                });
+                int lidx = (blocks - 1) * 214;
+                int llen = plaintext.Length - lidx;
+                byte[] lbuf = EncryptInternal(rsa, plaintext.TakeAt(lidx, llen));
+                Array.Copy(lbuf, 0, ciphertext, (blocks - 1) * 256, 256);
             }
+            return ciphertext;
         }
 
         private static byte[] EncryptInternal(RSA rsa, byte[] rgb)
@@ -84,14 +105,32 @@ namespace VSL.Crypt
         /// <exception cref="ArgumentNullException"></exception>
         /// <exception cref="ArgumentOutOfRangeException"></exception>
         /// <exception cref="CryptographicException"></exception>
-        /// <returns></returns>
         public static byte[] DecryptBlock(byte[] ciphertext, string key)
         {
             if (ciphertext == null) throw new ArgumentNullException(nameof(ciphertext));
             if (string.IsNullOrEmpty(key)) throw new ArgumentNullException(nameof(ciphertext));
             if (ciphertext.Length != 256) throw new ArgumentOutOfRangeException(nameof(ciphertext), ciphertext.Length, "One block must measure 256 bytes");
-            using (var rsa = CreateProvider(key))
+
+            return DecryptBlock(ciphertext, new RSAParameters().ImportXmlKey(key));
+        }
+
+        /// <summary>
+        /// Decrypts one block using RSA with OAEP.
+        /// </summary>
+        /// <param name="ciphertext">Data to decrypt as a single 256 byte block.</param>
+        /// <param name="key"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        /// <exception cref="CryptographicException"></exception>
+        public static byte[] DecryptBlock(byte[] ciphertext, RSAParameters key)
+        {
+            if (ciphertext == null) throw new ArgumentNullException(nameof(ciphertext));
+            if (ciphertext.Length != 256) throw new ArgumentOutOfRangeException(nameof(ciphertext), ciphertext.Length, "One block must measure 256 bytes");
+
+            using (RSA rsa = CreateProvider(key))
+            {
                 return DecryptInternal(rsa, ciphertext);
+            }
         }
 
         /// <summary>
@@ -102,12 +141,28 @@ namespace VSL.Crypt
         /// <exception cref="ArgumentNullException"/>
         /// <exception cref="ArgumentOutOfRangeException"/>
         /// <exception cref="CryptographicException"/>
-        /// <returns></returns>
         public static byte[] Decrypt(byte[] ciphertext, string key)
         {
             if (ciphertext == null) throw new ArgumentNullException(nameof(ciphertext));
             if (string.IsNullOrEmpty(key)) throw new ArgumentNullException(nameof(ciphertext));
             if (ciphertext.Length % 256 != 0) throw new ArgumentOutOfRangeException(nameof(ciphertext), ciphertext.Length % 256, "The blocksize must be 256 bytes");
+
+            return Decrypt(ciphertext, new RSAParameters().ImportXmlKey(key));
+        }
+
+        /// <summary>
+        /// Decrypts data using RSA with OAEP.
+        /// </summary>
+        /// <param name="ciphertext">Data to decrypt in multiple 256 byte blocks.</param>
+        /// <param name="key"></param>
+        /// <exception cref="ArgumentNullException"/>
+        /// <exception cref="ArgumentOutOfRangeException"/>
+        /// <exception cref="CryptographicException"/>
+        public static byte[] Decrypt(byte[] ciphertext, RSAParameters key)
+        {
+            if (ciphertext == null) throw new ArgumentNullException(nameof(ciphertext));
+            if (ciphertext.Length % 256 != 0) throw new ArgumentOutOfRangeException(nameof(ciphertext), ciphertext.Length % 256, "The blocksize must be 256 bytes");
+
             int blocks = ciphertext.Length / 256;
             byte[] tmp_plaintext = new byte[(blocks - 1) * 214];
             using (var rsa = CreateProvider(key))
@@ -135,7 +190,6 @@ namespace VSL.Crypt
         /// <summary>
         /// Generates a random RSA keypair as a <see cref="RSAParameters"/> struct.
         /// </summary>
-        /// <returns></returns>
         public static RSAParameters GenerateKeyPairParams()
         {
             using (RSA rsa = RSA.Create())
@@ -148,7 +202,6 @@ namespace VSL.Crypt
         /// <summary>
         /// Generates a random RSA keypair in xml format.
         /// </summary>
-        /// <returns></returns>
         public static string GenerateKeyPairXml()
         {
             using (RSA rsa = RSA.Create())
@@ -164,14 +217,14 @@ namespace VSL.Crypt
         /// <param name="privateKey">Keypair (xmlstring)</param>
         /// <exception cref="ArgumentNullException"></exception>
         /// <exception cref="CryptographicException"></exception>
-        /// <returns></returns>
         public static string ExtractPublicKey(string privateKey)
         {
             if (privateKey == null) throw new ArgumentNullException(nameof(privateKey));
 
-            using (RSA csp = CreateProvider(privateKey))
+            using (RSA rsa = RSA.Create())
             {
-                return csp.ExportParameters(false).ExportXmlKey();
+                rsa.ImportParameters(new RSAParameters().ImportXmlKey(privateKey));
+                return rsa.ExportParameters(false).ExportXmlKey();
             }
         }
 
@@ -180,27 +233,21 @@ namespace VSL.Crypt
         /// </summary>
         /// <param name="privateKey">Keypair (params)</param>
         /// <exception cref="CryptographicException"></exception>
-        /// <returns></returns>
         public static RSAParameters ExtractPublicKey(RSAParameters privateKey)
         {
-            using (var rsa = CreateProvider(privateKey))
+            using (RSA rsa = CreateProvider(privateKey))
+            {
                 return rsa.ExportParameters(false);
+            }
         }
         #endregion
         #region Util
-        private static RSA CreateProvider(string key)
-        {
-            RSA rsa = RSA.Create();
-            rsa.ImportParameters(new RSAParameters().ImportXmlKey(key));
-            return rsa;
-        }
         private static RSA CreateProvider(RSAParameters key)
         {
             RSA rsa = RSA.Create();
             rsa.ImportParameters(key);
             return rsa;
         }
-
         #endregion
     }
 }
