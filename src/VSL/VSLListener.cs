9669577c86adf9eb5b5a9f45b668f217dc68c47e
Daniel Lerch <daniel.p.lerch@gmail.com>
2018-09-14 15:53:09 +0200

    Create VSLListener class

---
+++ src/VSL/VSLListener.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace VSL
+{
+    public class VSLListener
+    {
+        private readonly Socket[] sockets;
+
+        public VSLListener(IPEndPoint[] addresses)
+        {
+            sockets = new Socket[addresses.Length];
+            for (int i = 0; i < addresses.Length; i++)
+            {
+                sockets[i] = new Socket(addresses[i].AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                sockets[i].Bind(addresses[i]);
+            }
+        }
+
+        public void Start()
+        {
+            foreach (Socket socket in sockets)
+            {
+                socket.Listen(Constants.ListenerBacklog);
+                
+            }
+        }
+
+        public void Stop()
+        {
+
+        }
+
+        private void Accept_Completed(object sender, SocketAsyncEventArgs e)
+        {
+            
+        }
+    }
+}
