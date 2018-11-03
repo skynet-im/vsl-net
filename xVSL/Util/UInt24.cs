df5d24fabfbf964d4b81bd37d860e3ee7aa95e1e
Daniel Lerch <daniel.p.lerch@gmail.com>
2018-11-03 15:57:08 +0100

    VSL 1.2.2.33 released with UInt24 fix

--- xVSL/Util/UInt24.cs
+++ xVSL/Util/UInt24.cs
@@ -10,12 +10,9 @@ namespace VSL
         internal static uint FromBytes(byte[] buf)
         {
             if (buf == null)
-                throw new ArgumentNullException("buf");
+                throw new ArgumentNullException(nameof(buf));
             if (buf.Length < 3)
-                throw new ArgumentOutOfRangeException("buf");
-            //return (((uint)(buf[0])) & 0x0000FF) |
-            //    (((uint)(buf[1]) << 8) & 0x00FF00) |
-            //    (((uint)(buf[2]) << 16) & 0xFF0000);
+                throw new ArgumentOutOfRangeException(nameof(buf), buf.Length, "The buffer must have a size of at least 3 bytes");
             return FromBytes(buf, 0);
         }
 
@@ -23,32 +20,32 @@ namespace VSL
         internal unsafe static uint FromBytes(byte[] buf, int startIdx)
         {
             if (buf == null)
-                throw new ArgumentNullException("buf");
-            if (buf.Length < 3)
-                throw new ArgumentOutOfRangeException("buf");
-            byte[] tmp = new byte[4];
-            fixed (byte* array = tmp)
+                throw new ArgumentNullException(nameof(buf));
+            if (startIdx < 0)
+                throw new ArgumentOutOfRangeException(nameof(startIdx), startIdx, "The start index must not be negative.");
+            if (startIdx > buf.Length - 3) // prevent buffer overflow
+                throw new ArgumentOutOfRangeException(nameof(startIdx), startIdx,
+                    "The start index must be small enough to keep three bytes remaining.");
+
+            byte* nn = stackalloc byte[4];
+            fixed (byte* buffer = buf)
             {
-                fixed (byte* buffer = buf)
+                if (BitConverter.IsLittleEndian)
                 {
-                    if (BitConverter.IsLittleEndian)
-                    {
-                        *(ushort*)array = *(ushort*)(buffer + startIdx);
-                        *(array + 2) = *(buffer + startIdx + 2);
-                    }
-                    else
-                    {
-                        *(ushort*)(array + 1) = *(ushort*)(buffer + startIdx);
-                        *(array + 3) = *(buffer + startIdx + 2);
-                    }
-                    return *(uint*)array;
+                    *(ushort*)nn = *(ushort*)(buffer + startIdx);
+                    *(nn + 2) = *(buffer + startIdx + 2);
                 }
+                else
+                {
+                    *(ushort*)(nn + 1) = *(ushort*)(buffer + startIdx);
+                    *(nn + 3) = *(buffer + startIdx + 2);
+                }
+                return *(uint*)nn;
             }
         }
 
         internal static byte[] ToBytes(uint n)
         {
-            //return new byte[] { (byte)n, (byte)(n >> 8), (byte)(n >> 16) };
             byte[] final = new byte[3];
             ToBytes(n, final, 0);
             return final;
@@ -57,18 +54,29 @@ namespace VSL
         [SecuritySafeCritical]
         internal unsafe static void ToBytes(uint n, byte[] buf, int startIdx)
         {
-            uint* nn = &n;
-            fixed (byte* array = buf)
+            if (buf == null)
+                throw new ArgumentNullException(nameof(buf));
+            if (startIdx < 0)
+                throw new ArgumentOutOfRangeException(nameof(startIdx), startIdx, "The start index must not be negative.");
+            if (startIdx > buf.Length - 3) // prevent buffer overflow
+                throw new ArgumentOutOfRangeException(nameof(startIdx), startIdx,
+                    "The start index must be small enough to keep three bytes remaining.");
+
+            byte* nn = (byte*)&n;
+            // This cast is necessary as (*uint)ptr + 1 is equal to &((uint*)ptr[1])
+            // which means we would actually skip sizeof(uint) instead of 1
+
+            fixed (byte* buffer = buf)
             {
                 if (BitConverter.IsLittleEndian)
                 {
-                    *(ushort*)(array + startIdx) = *(ushort*)nn;
-                    *(array + startIdx + 2) = *(byte*)(nn + 2);
+                    *(ushort*)(buffer + startIdx) = *(ushort*)nn;
+                    *(buffer + startIdx + 2) = *(nn + 2);
                 }
                 else
                 {
-                    *(ushort*)(array + startIdx) = *(ushort*)(nn + 1);
-                    *(array + startIdx + 2) = *(byte*)(nn + 3);
+                    *(ushort*)(buffer + startIdx) = *(ushort*)(nn + 1);
+                    *(buffer + startIdx + 2) = *(nn + 3);
                 }
             }
         }
