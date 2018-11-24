bdbc2081ba6ed1d422a4a859f89603b4b7282fd4
Daniel Lerch <daniel.p.lerch@gmail.com>
2018-11-24 19:16:12 +0100

    Fix cancellation of file transfer

--- src/VSL/VSLSocket.cs
+++ src/VSL/VSLSocket.cs
@@ -209,7 +209,7 @@ namespace VSL
             ConnectionAvailable = false;
             connectionLost = true;
             Channel.Shutdown();
-            // TODO: Cancel running file transfer
+            FileTransfer?.Dispose(); // Cancel running file transfer
             OnConnectionClosed(new ConnectionClosedEventArgs(reason, message, ex));
         }
         #endregion
@@ -226,34 +226,24 @@ namespace VSL
             {
                 if (disposing)
                 {
-                    // -TODO: dispose managed state (managed objects).
                     Channel?.Dispose();
                     Manager?.Dispose();
                 }
-
-                // -TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
-                // -TODO: set large fields to null.
 
                 disposedValue = true;
             }
         }
 
-        // -TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
         // ~VSLSocket() {
-        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
         //   Dispose(false);
         // }
-
-        // This code added to correctly implement the disposable pattern.
 
         /// <summary>
         /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
         /// </summary>
         public void Dispose()
         {
-            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
             Dispose(true);
-            // -TODO: uncomment the following line if the finalizer is overridden above.
             // GC.SuppressFinalize(this);
         }
         #endregion
