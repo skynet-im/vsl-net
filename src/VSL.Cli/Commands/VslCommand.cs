952aa1f0c9b2ef6114a56b8a5aa40789daa0ca31
Daniel Lerch <daniel.p.lerch@gmail.com>
2019-02-26 22:04:22 +0100

    Add VSL debug/test console

---
+++ src/VSL.Cli/Commands/VslCommand.cs
@@ -0,0 +1,18 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VSL.Cli.Commands
+{
+    [Command("vsl", Description = "VSL Debugging and Test console")]
+    [Subcommand(typeof(PentestCommand))]
+    internal class VslCommand
+    {
+        private int OnExecute(CommandLineApplication app)
+        {
+            app.ShowHelp();
+            return 1;
+        }
+    }
+}
---
+++ src/VSL.Cli/Program.cs
@@ -0,0 +1,27 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using VSL.Cli.Commands;
+
+namespace VSL.Cli
+{
+    class Program
+    {
+        static async Task<int> Main(string[] args)
+        {
+            if (Debugger.IsAttached)
+            {
+                Console.Write("VSL is running in debug mode. Please enter your command: ");
+                args = Console.ReadLine().Split(' ');
+                Console.WriteLine();
+                int result = await CommandLineApplication.ExecuteAsync<VslCommand>(args);
+                Console.WriteLine();
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey(true);
+                return result;
+            }
+            else return await CommandLineApplication.ExecuteAsync<VslCommand>(args);
+        }
+    }
+}
